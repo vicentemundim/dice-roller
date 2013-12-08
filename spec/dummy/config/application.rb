25656d07e0f0d76bf898f2f6b790c5b8f2261b71
Vicente Mundim <vicente.mundim@gmail.com>
2013-12-08 18:55:38 -0200

    Preparing assets for generating compiled js and css files

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -53,6 +53,8 @@ module Dummy
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
 
+    config.assets.precompile += ['dependencies.js', 'dice-roller.js', 'example.js', 'dice-roller.css', 'example.css']
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
